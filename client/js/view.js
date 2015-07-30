744aca475f8121fc74395ed06e7dc81d7042d413
jluo <jluo@q.com>
2015-07-30 18:30:59 +0800

    client ok

--- client/js/view.js
+++ client/js/view.js
@@ -1,46 +1,50 @@
 (function () {
 	var MAX = 14;
-	var card = {
-		'east',
-		'west',
-		'south',
-		'north',
-		'zhong',
-		'fa',
-		'bai',
-		'dot1',
-		'dot2',
-		'dot3',
-		'dot4',
-		'dot5',
-		'dot6',
-		'dot7',
-		'dot8',
-		'dot9',
-		'Bamboo1',
-		'Bamboo2',
-		'Bamboo3',
-		'Bamboo4',
-		'Bamboo5',
-		'Bamboo6',
-		'Bamboo7',
-		'Bamboo8',
-		'Bamboo9',
-		'Char1',
-		'Char2',
-		'Char3',
-		'Char4',
-		'Char5',
-		'Char6',
-		'Char7',
-		'Char8',
-		'Char9'
-	};
+	var tags = new Array(
+			'east',
+			'west',
+			'south',
+			'north',
+			'zhong',
+			'fa',
+			'bai',
+			'dot1',
+			'dot2',
+			'dot3',
+			'dot4',
+			'dot5',
+			'dot6',
+			'dot7',
+			'dot8',
+			'dot9',
+			'Bamboo1',
+			'Bamboo2',
+			'Bamboo3',
+			'Bamboo4',
+			'Bamboo5',
+			'Bamboo6',
+			'Bamboo7',
+			'Bamboo8',
+			'Bamboo9',
+			'Char1',
+			'Char2',
+			'Char3',
+			'Char4',
+			'Char5',
+			'Char6',
+			'Char7',
+			'Char8',
+			'Char9');
 	var
 	DISCARD = 0x20000001,
 	DISCARD_PONG = 0x30000001,
 	DISCARD_CHI = 0x30000002,
 	DISCARD_DRAW = 0x30000003;
+	var Data = function () {
+		this.handcards = new Array(),
+		this.discard = tags.length, //transparent material
+		this.draw = tags.length;
+	}
 	function View() {
 		this.cards = new Array();
 		this.cardMats = new Array();
@@ -48,113 +52,126 @@
 		this.draw = null;
 		this.canvas = null;
 		this.scene = null;
-		this.handcards = new Array();
+		this.data = new Data();		
 
 		// Resize
 		//window.addEventListener("resize", function () {
 		//	this.engine.resize();
 		//});
-	}
-	View.prototype.dealCard = function (cards) {
-		this.handcards = cards;
-	}
-	View.prototype.insCard = function (draw) {
-		this.discard.material = this.cardMats[draw];
-
-		for (i = 0; i < MAX && draw > this.handcards[i]; i++);
-
-		for (j = MAX - 1; j > i; j--)
-			this.handcards[j + 1] = this.handcards[j];
-
-		this.handcards[i] = draw;
-	};
-	View.prototype.disCard = function (discard) {
-		console.log("disCard " + discard);
-		for (key : handcards) {
-			this.cards[i].material = this.cardMats[key];
+		this.dealCard = function (cards) {
+			this.data.draw = tags.length;
+			this.data.discard = tags.length;
+			this.data.handcards = cards;
 		}
-	};
-	View.prototype.command = function (cmd, cardid) {};
-	View.prototype.who = function (hiscards, isLosed) {
-		for (key : hiscards) {
-			var card = BABYLON.Mesh.CreatePlane('hiscard' + i, 5, this.scene);
-			card.position.x = 20 * i;
-			card.position.y = 20;
-			card.position.z = 10;
-			card.rotation.z = Math.PI;
-			card.material.specularColor = new BABYLON.Color3(0, 0, 0);
-			card.material = this.cardMats[key];
-		}
-		if (isLosed) {}
-	};
-	View.prototype.open = function () {};
-	View.prototype.create = function (canvas) {
-		this.cards = new Array();
-		this.cardMats = new Array();
-		this.discard = null;
-		this.draw = null;
-		this.canvas = canvas;
-		var engine = new BABYLON.Engine(canvas, true);
-		this.scene = new BABYLON.Scene(engine);
-		var light = new BABYLON.PointLight("Point", new BABYLON.Vector3(5, 10, 5), this.scene);
-		var camera = new BABYLON.ArcRotateCamera("Camera", 1, 0.8, 8, new BABYLON.Vector3(0, 0, 0), this.scene);
+		this.insCard = function (draw) {
+			this.discard.material = this.cardMats[draw];
 
-		this.handcards = new Array();
-		
-		start();
+			for (i = 0; i < MAX && draw > this.handcards[i]; i++);
 
-		engine.runRenderLoop(function () {
-			this.scene.render();
-		});
-	};
-	View.prototype.start = function () {
-		for (i = 0; i < MAX; i++) {
-			this.cards[i] = BABYLON.Mesh.CreatePlane('card' + i, 5, this.scene);
-			this.cards[i].position.x = 20 * i;
-			this.cards[i].position.y = -10;
-			this.cards[i].position.z = 10;
-			this.cards[i].material.specularColor = new BABYLON.Color3(0, 0, 0);
+			for (j = MAX - 1; j > i; j--)
+				this.handcards[j + 1] = this.handcards[j];
 
-			this.cards[i].actionManager = new BABYLON.ActionManager(scene);
-			this.cards[i].actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function (evt) { // chu card
+			this.handcards[i] = draw;
+		};
+		this.disCard = function (discard) {
+			console.log("disCard " + discard);
+			this.cards[discard].material = null;
+		};
+		this.who = function (hiscards, isLosed) {
+			for (key in hiscards) {
+				var card = BABYLON.Mesh.CreatePlane('hiscard' + i, 5, this.scene);
+				card.position.x = 20 * i;
+				card.position.y = 20;
+				card.position.z = -10;
+				card.rotation.z = Math.PI;
+				card.material = this.cardMats[key];
+			}
+			if (isLosed) {}
+		};
+		this.create = function (canvas) {
+			var _this = this;
+			_this.cards = new Array();
+			_this.cardMats = new Array();
+			_this.discard = null;
+			_this.draw = null;
+			_this.canvas = canvas;
+			var engine = new BABYLON.Engine(canvas, true);
+			this.scene = new BABYLON.Scene(engine);
+			this.scene.clearColor = new BABYLON.Color3(35/255.0, 116/255.0, 172/255.0);
+			var light = new BABYLON.PointLight("Point", new BABYLON.Vector3(3 * MAX, 0, -80), _this.scene);
+			var camera = new BABYLON.FreeCamera("Camera", new BABYLON.Vector3(3 * MAX, 0, -60), _this.scene);
+			//var camera = new BABYLON.ArcRotateCamera('Camera', 1, .8, 10, new BABYLON.Vector3(0, 0, 0), scene);
+			_this.start();
+			
+			this.scene.activeCamera.attachControl(canvas);
+
+			engine.runRenderLoop(function () {
+				_this.scene.render();
+			});
+			return this;
+		};
+		this.start = function () {
+			var _this = this;
+			for (var i = 0; i < MAX; i++) {
+				_this.cards[i] = BABYLON.Mesh.CreatePlane('card' + i, 5, _this.scene);
+				_this.cards[i].position.x = 6 * i;
+				_this.cards[i].position.y = -20;
+				_this.cards[i].position.z = 0;
+				//_this.cards[i].rotation.x = Math.PI/12;
+				_this.cards[i].actionManager = new BABYLON.ActionManager(_this.scene);
+				_this.cards[i].actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function (evt) { // chu card
+						var card = parseInt(evt.source.material.name, 10);
+						_this.disCard(i);
+						_this.command(DISCARD, card);
+					}));
+			}
+			_this.discard = BABYLON.Mesh.CreatePlane('discard', 5, _this.scene);
+			_this.discard.position.x = 6 * (i-3);
+			_this.discard.position.y = -10;
+			_this.discard.position.z = 0;
+			//_this.discard.rotation.x = Math.PI/12;
+			_this.discard.actionManager = new BABYLON.ActionManager(_this.scene);
+			_this.discard.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function (evt) { //chi card/pong card
 					var i = parseInt(evt.source.material.name, 10);
-					disCard(i);
-					command(DISCARD, i);
+					_this.invalidate();
+					_this.command(DISCARD_PONG, i);
 				}));
-		}
-		this.discard = BABYLON.Mesh.CreatePlane('discard', 5, this.scene);
-		this.discard.position.x = 20 * i;
-		this.discard.position.y = 0;
-		this.discard.position.z = 10;
-		this.discard.material.specularColor = new BABYLON.Color3(0, 0, 0);
-		this.discard.actionManager = new BABYLON.ActionManager(scene);
-		this.discard.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function (evt) { //chi card/pong card
-				var i = parseInt(evt.source.material.name, 10);
-				invalidate();
-				command(DISCARD_PONG, i);
-			}));
-		this.draw = BABYLON.Mesh.CreatePlane('draw', 5, this.scene);
-		this.draw.position.x = 20 * i;
-		this.draw.position.y = 0;
-		this.draw.position.z = 10;
-		this.draw.material.specularColor = new BABYLON.Color3(0, 1, 0);
-		this.discard.actionManager = new BABYLON.ActionManager(scene);
-		this.discard.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function (evt) { // draw card
-				var i = parseInt(evt.source.material.name, 10);
-				command(DISCARD_DRAW, i);
-			}));
+			_this.draw = BABYLON.Mesh.CreatePlane('draw', 5, _this.scene);
+			_this.draw.position.x = 6 * (i-5);
+			_this.draw.position.y = -10;
+			_this.draw.position.z = 0;
+			//_this.draw.rotation.x = Math.PI*2;
+			_this.draw.actionManager = new BABYLON.ActionManager(_this.scene);
+			_this.draw.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function (evt) { // draw card
+					var i = parseInt(evt.source.material.name, 10);
+					command(DISCARD_DRAW, i);
+				}));
 
-		for (key : card) {
-			this.cardMats[key] = new BABYLON.StandardMaterial('00' + key, this.scene);
-			this.cardMats[key].diffuseTexture = new BABYLON.Texture('./asserts/Mahjong/' + card[key] + '.png', this.scene);
-		}
-	};
-	View.prototype.invalidate = function () {
-		this.discard.material = null;
-		for (key : this.handcards) {
-			this.cards[i].material = this.cardMats[key];
-		}
-	};
+			_this.cardMats[tags.length] = new BABYLON.StandardMaterial('00' + tags.length, _this.scene);
+			_this.cardMats[tags.length].alpha = 0.0;        
+			for (key in tags) {
+				_this.cardMats[key] = new BABYLON.StandardMaterial('00' + (key+1), _this.scene);
+				_this.cardMats[key].diffuseTexture = new BABYLON.Texture('./asserts/Mahjong/' + tags[key] + '.png', _this.scene);
+				_this.cardMats[key].diffuseTexture.hasAlpha = true;
+				_this.cardMats[key].specularColor = new BABYLON.Color3(0, 0, 0);
+			}
+		};
+		this.invalidate = function () {
+			this.draw.material = this.cardMats[this.data.draw];
+			this.discard.material = this.cardMats[this.data.discard];
+			var i = 0;
+			for (key in this.data.handcards) {
+				this.cards[i].material = this.cardMats[key];
+				i ++;
+			}
+			for( i = this.data.handcards.length ; i < MAX ; i ++ ) {
+				this.cards[i].material = this.cardMats[tags.length];
+			}
+		};
+	}
+	View.prototype.command = function (cmd, cardid) {};
+
+	View.prototype.open = function () {};
 
 	if (typeof exports !== "undefined" && exports !== null) {
 		exports.View = new View();
