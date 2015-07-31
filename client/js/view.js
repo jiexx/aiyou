f37d0f840b5f5d760b45668db2b907579b237977
roger <jiexx@outlook.com>
2015-08-01 00:46:05 +0800

    pongchi to be debug

--- client/js/view.js
+++ client/js/view.js
@@ -36,10 +36,10 @@
 			'Char8',
 			'Char9', //33
 			'back');
-	var transparent = tags.length;
+	//var transparent = tags.length;
 	var background = tags.length + 1;
 	var back = tags.length - 1;
-	var INVALIDCARD = 0xff,
+	var INVALIDCARD = tags.length,
 	CARDSIZE = 9;
 	var
 	DISCARD = 0x20000001,
@@ -50,7 +50,7 @@
 	var Data = function () {
 		this.cards = new Array();
 		this.showcards = new Array();
-		this.discard = transparent;
+		this.discard = INVALIDCARD;
 	};
 	function fix(oo) {
 	
@@ -64,29 +64,39 @@
 	}
 	function Cards() {
 		this.cards = new Array();
+		this.card_y = 0;
 		this.showcards = new Array();
 		this.pump = new Array();
 		this.discard = null;
 		this.data = new Data();
 		this.needUpdate = true;
-		this.disCard = function (pos, m) {
+		this.myself = null;
+		this.pumpRestore = function() {
+			for( var k in this.pump ) 
+				this.cards[this.pump[k]].position.y = this.card_y;
+			this.pump.splice(0, this.pump.length);
+			this.needUpdate = true;
+		}
+		this.disCard = function (pos, no) {
 			var i;
 			for (i = pos; i < this.data.cards.length; i++) {
 				this.data.cards[i] = this.data.cards[i + 1];
 			}
 			if( i == this.data.cards.length ) 
-				this.data.cards[i] = transparent;
+				this.data.cards[i-1] = INVALIDCARD;
 			
-			this.data.discard = m;
+			this.data.discard = no;
 			this.needUpdate = true;
 		}
 		this.pongchi = function (i, j, k) {
 			this.data.showcards.push(i);
 			this.data.showcards.push(j);
 			this.data.showcards.push(k);
+			this.needUpdate = true;
 		}
 		this.start = function (scene, card_y, showcard_y, discard_y, isCardCB, isDiscardCB) {
-			_this = this;
+			var _this = this;
+			_this.card_y = card_y;
 			var i;
 			for (i = 0; i < MAX; i++) {
 				_this.cards[i] = BABYLON.Mesh.CreatePlane('000' + i, CARDSIZE, scene);
@@ -100,20 +110,23 @@
 					_this.cards[i].actionManager = new BABYLON.ActionManager(scene);
 					_this.cards[i].actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function (evt) { // chu card
 							if (evt.source.material != null) {
-								var pos = parseInt(evt.source.id, 10);
-								var mat = parseInt(evt.source.material.name, 10);
-								console.log("disCard " + pos);
+								var pos = parseInt(evt.source.name, 10);
+								var no = parseInt(evt.source.material.name, 10);
 								if (evt.source.position.y == card_y) {
+								//----------------------------pump------------------------
 									evt.source.position.y += 3;
-									_this.pump.push(i);
-								} else if (evt.source.position.y == card_y + 3) {
-									evt.source.position.y = card_y;
-									_this.disCard(pos, mat);
-									for (var k in _this.pump) {
-										if (_this.pump[k] == i)
-											_this.pump.splice(k, 1);
+									_this.pump.push(pos);
+								} else if (evt.source.position.y == card_y + 3 && no != INVALIDCARD ) {
+								//----------------------------discard------------------------
+									if( _this.pump.length == 1 && _this.data.cards.length == MAX - 1 ) {
+										evt.source.position.y = card_y;
+										_this.disCard(pos, no);
+										_this.pumpRestore();
+										_this.__proto__.command(DISCARD, no);
+									}else {
+										evt.source.position.y = _this.card_y;
+										_this.pump.splice(pos, 1);
 									}
-									_this.__proto__.command(DISCARD, i);
 								}
 							}
 						}));
@@ -138,20 +151,23 @@
 				_this.discard.actionManager = new BABYLON.ActionManager(scene);
 				_this.discard.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function (evt) { //chi card/pong card
 						if (evt.source.material != null) {
-							var i = parseInt(evt.source.material.name, 10);
-							console.log("draw " + evt.source.material.name);
-							if (_this.pump.length == 2) {
-								if (_this.pump[0] == _this.pump[1] && _this.pump[0] == i) {
-									_this.command(DISCARD_PONG, i);
-									Cards.pongchi(_this.pump[0], _this.pump[1], i);
-								} else if (Cards.chi(_this.pump[0], _this.pump[1], i)) {
-									_this.command(DISCARD_CHI, i);
-									Cards.pongchi(_this.pump[0], _this.pump[1], i);
+							var no = parseInt(evt.source.material.name, 10);
+							//----------------------------multi draw------------------------
+							var that = _this.myself;
+							if (that != null && that.pump.length == 2) {
+								var p0 = parseInt(that.cards[that.pump[0]].material.name, 10);
+								var p1 = parseInt(that.cards[that.pump[1]].material.name, 10);
+								if (p0 == p1 && p1 == no) {
+									that.command(DISCARD_PONG, no);
+									that.pongchi(p0, p1, no);
+								} else if (that.chi(p0, p1, no)) {
+									that.command(DISCARD_CHI, no);
+									that.pongchi(p0, p1, no);
 								} else {
-									Cards.warning();
+									_this.warning();
 								}
 							} else {
-								Cards.warning();
+								_this.warning();
 							}
 						}
 					}));
@@ -159,13 +175,13 @@
 		}
 		this.invalidate = function (scene, materials) {
 			if( this.needUpdate ) {
-				var i = 0;
+				var i = 0, key;
 				for (key in this.data.cards) {
-					this.cards[i].material = materials[key];
+					this.cards[i].material = materials[this.data.cards[key]];
 					i++;
 				}
 				for (i = this.data.cards.length; i < MAX; i++) {
-					this.cards[i].material = materials[transparent];
+					this.cards[i].material = materials[INVALIDCARD];
 				}
 				i = 0;
 				for (key in this.data.showcards) {
@@ -173,7 +189,7 @@
 					i++;
 				}
 				for (i = this.data.showcards.length; i < MAX; i++) {
-					this.showcards[i].material = materials[transparent];
+					this.showcards[i].material = materials[INVALIDCARD];
 				}
 				this.discard.material = materials[this.data.discard];
 				this.needUpdate = false;
@@ -226,7 +242,7 @@
 			this.cardDraw = round_dealcard;
 		};
 		this.himDiscard = function (him_discard) {
-			console.log("disCard " + discard);
+			console.log("disCard " + him_discard);
 			this.hiscards.data.discard = him_discard;
 			this.invalidate();
 		};
@@ -253,9 +269,9 @@
 		};
 		this.start = function () {
 			var _this = this;
-			_this.cardMats[transparent] = new BABYLON.StandardMaterial('00' + tags.length, _this.scene);
+			_this.cardMats[INVALIDCARD] = new BABYLON.StandardMaterial('00' + INVALIDCARD, _this.scene);
 			//_this.cardMats[transparent].specularColor = new BABYLON.Color3(0, 0, 0);
-			_this.cardMats[transparent].alpha = 0.0;
+			_this.cardMats[INVALIDCARD].alpha = 0.0;
 			for (key in tags) {
 				_this.cardMats[key] = new BABYLON.StandardMaterial('00' + key, _this.scene);
 				_this.cardMats[key].diffuseTexture = new BABYLON.Texture('./asserts/Mahjong/' + tags[key] + '.png', _this.scene/*, true, true, BABYLON.Texture.NEAREST_SAMPLINGMODE, function () {
@@ -271,6 +287,7 @@
 
 			_this.mycards.start(_this.scene, -25, -15, -5, true, false);
 			_this.hiscards.start(_this.scene, 25, 15, 5, false, true);
+			_this.hiscards.myself = _this.mycards;
 
 			_this.draw = BABYLON.Mesh.CreatePlane('draw', CARDSIZE, _this.scene);
 			_this.draw.position.x = 6 * (MAX - 5);
@@ -278,12 +295,13 @@
 			_this.draw.position.z = 0;
 			//_this.draw.rotation.x = Math.PI*2;
 			//fix(_this.draw);
+			//----------------------------single draw------------------------
 			_this.draw.actionManager = new BABYLON.ActionManager(_this.scene);
 			_this.draw.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function (evt) { // draw card
-					if (cardDraw != INVALIDCARD && _this.mycards.data.length == MAX - 1) {
-						_this.mycards.data[MAX] = cardDraw;
+					if (_this.cardDraw != INVALIDCARD && _this.mycards.data.length == MAX - 1) {
+						_this.mycards.data[MAX] = _this.cardDraw;
 					} else {
-						Cards.warning();
+						_this.mycards.warning();
 					}
 				}));
 			_this.draw.material = _this.cardMats[back];
@@ -295,12 +313,11 @@
 			_this.desk.position.z = 10;
 			//_this.draw.rotation.x = Math.PI*2;
 			//fix(_this.desk);
+			//----------------------------undiscard------------------------
 			_this.desk.actionManager = new BABYLON.ActionManager(_this.scene);
 			_this.desk.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function (evt) { // draw card
-					if (cardDraw != INVALIDCARD && _this.mycards.data.length == MAX - 1) {
-						_this.mycards.data[MAX] = cardDraw;
-					} else {
-						Cards.warning();
+					if (_this.mycards.pump.length != 0) {
+						_this.mycards.pumpRestore();
 					}
 				}));
 			_this.desk.material = _this.cardMats[background];
