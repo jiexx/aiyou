42becc209a01a3fd0310b3f479e82613e0a1fce3
unknown <jluo.roger@jluo.qinmaohao.com>
2015-11-10 19:35:28 +0800

    android fmk

--- android/src/com/jiexx/aiyou/LoadingActivity.java
+++ android/src/com/jiexx/aiyou/LoadingActivity.java
@@ -2,9 +2,6 @@ package com.jiexx.aiyou;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.rest.RestService;
-import org.androidannotations.annotations.sharedpreferences.Pref;
-
 import android.os.Bundle;
 import android.app.Activity;
 
