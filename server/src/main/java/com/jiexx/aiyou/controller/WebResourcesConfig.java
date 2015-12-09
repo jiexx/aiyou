93d275c7852d5e1ae03233e4eb81b5bdbe094324
unknown <jluo.roger@jluo.qinmaohao.com>
2015-12-09 14:14:52 +0800

    images upload: save into images folder next to server.jar

--- server/src/main/java/com/jiexx/aiyou/controller/WebResourcesConfig.java
+++ server/src/main/java/com/jiexx/aiyou/controller/WebResourcesConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebResourcesConfig extends WebMvcConfigurerAdapter {
 	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
 			"classpath:/META-INF/resources/", "classpath:/resources/",
-			"classpath:/static/", "classpath:/public/", "classpath:/conf/", "classpath:/version/" };
+			"classpath:/static/", "classpath:/public/", "classpath:/conf/", "classpath:/version/", "file:"+System.getProperty("user.dir") + "/images/" };
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
     	if (!registry.hasMappingForPattern("/webjars/**")) {
@@ -20,5 +20,9 @@ public class WebResourcesConfig extends WebMvcConfigurerAdapter {
     		registry.addResourceHandler("/**").addResourceLocations(
     				CLASSPATH_RESOURCE_LOCATIONS);
     	}
+    	//System.out.println("file:"+System.getProperty("user.dir") + "/images/");
+    	/*if (!registry.hasMappingForPattern("/images/**")) {
+    		registry.addResourceHandler("/images/**").addResourceLocations("file:"+System.getProperty("user.dir") + "\\images\\");
+    	}*/
     }
 }
