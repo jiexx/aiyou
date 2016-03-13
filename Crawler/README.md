127410b787c2eb93a7575e305e084c819127dcd3
roger <jiexx@outlook.com>
2016-03-13 21:33:37 +0800

    crawler xpath

--- Crawler/README.md
+++ Crawler/README.md
@@ -2,3 +2,6 @@
 2.casperjs 1.1.0 beta5
 3.nodejs v4.4.0
 4.phantomjs 2.1.1
+
+http://www.w3school.com.cn/xpath/xpath_syntax.asp
+http://docs.casperjs.org/en/latest/modules/casper.html#getelementsattribute
--- Crawler/browser.js
+++ Crawler/browser.js
@@ -28,46 +28,62 @@ var id = system.args[1];
 var link = system.args[2];
 var type = system.args[3];
 
+// for redirect page
+var xpathFetch = '//div[contains(@id,"result_")]/div/div[0]/div/div/a';
+var xpathRedirect = '//div[@id="pagn")]/span[@class="pagnLink"]/a';
+var xpathImage = '//div[contains(@id,"result_")]/div/div[0]/div/div/a/img';
+
+//for fetch page
+var xpathDesc = '//div[@id="productDescription")]/div[@class="content"]/div[@class="productDescriptionWrapper"]';
+
 browser.start();  
 
 browser.open(system.args[1]);  
   
-browser.thenEvaluate(function out() {  
-	var x = require('casper').selectXPath;
-	if(type == 'fetch') {
-		var pathImage = '//*[@id="landingImage"]';
-		var image = this.getElement(x(pathImage), 'src');
-		var pathFetch = '//*[@id="mainResults"]/ul/li[0]/div[0]';
-		var linkFetch = this.getElement(x(pathFetch), 'src');
-		var pathUpdate = '//*[@class="pagnLink"]';
-		var linkUpdate = this.getElement(x(pathUpdate), 'src');
+browser.thenEvaluate(function out() {
+	var info;
+	if(type == 'fetch') {  // can be 
+		var descInfo = this.getElementInfo(xpathDesc);
 		
 		browser.open('http://127.0.0.1/update', {
 		    method: 'post',
 		    data:   {
 		    	'id': id,
 		    	'type': 'fetch',
-		        'fetchLinks': linkFetch,
-		        'redirectLinks':  linkUpdate,
-		        'name': documment.title,
-		        'image': image.src
+		        'desc': descInfo,
 		    },
 		});
 	}else if(type == 'redirect') {
-		var pathFetch = '//*[@id="mainResults"]/ul/li[0]/div[0]';
-		var linkFetch = this.getElement(x(pathFetch), 'src');
-		var pathUpdate = '//*[@class="pagnLink"]';
-		var linkUpdate = this.getElement(x(pathUpdate), 'src');
+		var imagesInfo = this.getElementInfo(xpathImage);
+		var fetchInfo = this.getElementInfo(xpathFetch);
+		var redirectInfo = this.getElementInfo(xpathUpdate);
+		
+		var linksImage = [];
+		for(info in imagesInfo) {
+			linksImage.push(info.attributes.src);
+		}
+		
+		var linksFetch = [];
+		var titlesFetch = [];
+		for(info in fetchInfo) {
+			linksFetch.push(info.attributes.href);
+			titlesFetch.push(info.attributes.title)
+		}
+		
+		var linksRedirect = [];
+		for(info in redirectInfo) {
+			linksRedirect.push(info.attributes.href);
+		}
 		
 		browser.open('http://127.0.0.1/update', {
 		    method: 'post',
 		    data:   {
 		    	'id': id,
-		    	'type': 'redirect',
-		        'linkFetch': linkFetch,
-		        'linkUpdate':  linkUpdate,
-		        'name': documment.title,
-		        'image': image.src
+		    	'type': 'fetch',
+		        'fetchLinks': linksFetch,
+		        'redirectLinks':  linksRedirect,
+		        'name': titlesFetch,
+		        'image': linksImage
 		    },
 		});
 	}
