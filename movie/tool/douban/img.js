5ccb9be73fa77a9fbca84058c0e811b34b546d43
unknown <jluo.Roger@jluo-new1.qinmaohao.com>
2016-06-14 15:40:05 +0800

    movie kickass sub /img download

--- movie/tool/douban/img.js
+++ movie/tool/douban/img.js
@@ -11,7 +11,7 @@ var connection = mysql.createConnection({
 	password : '1234',
 });
 var dbReady = false;
-
+var db_table = 'amazon.movie';
 
 var fs = require("fs");
 var request = require('request');
@@ -54,7 +54,11 @@ var down2 = function(url, filename, cb) {
   request.end();
 } 
 
-function download(uri, filename, callback){
+function isJson(text) {
+	return /^[\],:{}\s]*$/.test(text.replace(/\\["\\\/bfnrtu]/g, '@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').replace(/(?:^|:|,)(?:\s*\[)+/g, ''));
+}
+
+function download(item, filename, callback){
 	/*request.get(uri, function(err, res, body){
 		console.log('download err:'+err +' res:'+res.statusCode);
 		if(!err && res && res.statusCode == 200) {
@@ -84,32 +88,45 @@ function download(uri, filename, callback){
 		},
 		hostname:"movie.douban.com",
 		agent:false,
-		path: uri,
+		path: item.uri,
 		method: 'GET',
 		agentOptions: {
 			secureProtocol: 'SSLv3_method'
 		}
 	};
+	console.log('download json:'+ JSON.stringify(item));
 	var req = https.request(options, function(res) {
 		//console.log("statusCode: ", res.statusCode);
 		//console.log("headers: ", res.headers);
 		var data = '';
 		res.setEncoding('utf8');
 		res.on('data', function (chunk) {
-			console.log(chunk);
+			console.log('chunk:'+chunk);
 			data += chunk;
 		});
 
 		res.on('end', function(){
-			var result = JSON.parse(data);
-			if(result[0] && result[0].img) {
-				var img = result[0].img;
-				var real = /*'https://img3.doubanio.com*/'/view/photo/photo/public/'+img.substring(img.lastIndexOf('/')+1);
-				var mime = '.jpg';//img.substr(img.lastIndexOf('.'));
-				down2(real, filename+mime, callback);
-			}
-			else{
+			if(!isJson(data)) {
+				fs.appendFile('img.txt', 'ERR_JSON_IMG '+uri+'\n', 'utf-8', function (err) {});
 				callback();
+				return;
+			}else {
+				console.log('end '+data + ' '+item.uri);
+				var result = JSON.parse(data);
+				if(result && result[0] && result[0].img) {
+					var img = result[0].img;
+					var val = [result[0].title,item.id];
+					connection.query('update '+db_table+' set subtitle=? where id=? ; ', val, function(err, results, fields) {
+						console.log('update '+result[0].title);
+					});
+					var real = /*'https://img3.doubanio.com*/'/view/photo/photo/public/'+img.substring(img.lastIndexOf('/')+1);
+					var mime = '.jpg';//img.substr(img.lastIndexOf('.'));
+					console.log('image: '+'https://img3.doubanio.com/view/photo/photo/public/'+img.substring(img.lastIndexOf('/')+1));
+					down2(real, filename+mime, callback);
+				}
+				else{
+					callback();
+				}
 			}
 		});
 	});
@@ -123,49 +140,52 @@ function getURLParameter(uri, name) {
 }
 
 function imgDownload(set, i){
-	console.log(' rows:'+set[i].uri);
-	download(set[i].uri, __dirname+'/'+'img/'+set[i].id, function(){
+	console.log(' rows:'+JSON.stringify(set[i]));
+	var imgFile = __dirname+'/'+'img/'+set[i].id+'.jpg';
+	download(set[i], imgFile, function(){
 		console.log('callback '+set[i].uri);
-		
-		console.log(__dirname+'\\'+'img\\'+set[i].id+'.jpg');
-		im.open(__dirname+'\\'+'img\\'+set[i].id+'.jpg', function(err, image){
-			if(!image){
-				fs.appendFile('img.txt', 'ERR_THUMB_IMG '+set[i].id+'\n', 'utf-8', function (err) {});
-			}
-			var h = (200.0/ image.width() ) * image.height();
-			image.batch()
-			.resize(200, h)
-			.writeFile(__dirname+'\\'+'thumb\\'+set[i].id+'.jpg',function(err, image){
-				//connection.query('update amazon.xunleitai set clazz="ka" where id="'+set[i].id+'" ; ', function(err, results, fields) {
-					if (!err) console.log('resized done:'+__dirname+'\\'+'thumb\\'+set[i].id+'.jpg ');
-					else console.log('resized err:'+__dirname+'\\'+'thumb\\'+set[i].id+'.jpg '+err);
-				//});
-			});
-		});
-		/*im.resize({
-			srcPath: __dirname+'\\'+'img\\'+set[i].id+'.jpg',
-			dstPath: __dirname+'\\'+'thumb\\'+set[i].id+'.jpg',
-			width: 200
-		}, function(err, stdout, stderr){
-			//if(!err) {
-				connection.query('update amazon.xunleitai set clazz="ka" where id="'+set[i].id+'" ; ', function(err, results, fields) {
-					if (!err) console.log('resized done:'+__dirname+'\\'+'thumb\\'+set[i].id+'.jpg ');
-					else console.log('resized err:'+__dirname+'\\'+'thumb\\'+set[i].id+'.jpg '+err);
+		if (fs.existsSync(imgFile)) {
+			console.log('image down file:'+imgFile);
+			im.open(imgFile, function(err, image){
+				if(!image){
+					fs.appendFile('img.txt', 'ERR_THUMB_IMG '+set[i].id+'\n', 'utf-8', function (err) {});
+					return;
+				}
+				var h = (200.0/ image.width() ) * image.height();
+				image.batch()
+				.resize(200, h)
+				.writeFile(__dirname+'\\'+'thumb\\'+set[i].id+'.jpg',function(err, image){
+					connection.query('update '+db_table+' set clazz="ka" where id="'+set[i].id+'" ; ', function(err, results, fields) {
+						if (!err) console.log('resized done:'+__dirname+'\\'+'thumb\\'+set[i].id+'.jpg ');
+						else console.log('resized err:'+__dirname+'\\'+'thumb\\'+set[i].id+'.jpg '+err);
+					});
 				});
-			//}
-		});*/
-		if(i >= 0) {
-			setTimeout(function(){imgDownload(set, i-1);},1500);
-			
-		} 
-	
+			});
+			/*im.resize({
+				srcPath: __dirname+'\\'+'img\\'+set[i].id+'.jpg',
+				dstPath: __dirname+'\\'+'thumb\\'+set[i].id+'.jpg',
+				width: 200
+			}, function(err, stdout, stderr){
+				//if(!err) {
+					connection.query('update amazon.xunleitai set clazz="ka" where id="'+set[i].id+'" ; ', function(err, results, fields) {
+						if (!err) console.log('resized done:'+__dirname+'\\'+'thumb\\'+set[i].id+'.jpg ');
+						else console.log('resized err:'+__dirname+'\\'+'thumb\\'+set[i].id+'.jpg '+err);
+					});
+				//}
+			});*/
+		}
+		if(i >= 1) {
+			setTimeout(function(){imgDownload(set, i-1);},5);
+		}else {
+			console.log('FINISHED');
+		}
 	});
 }
 
 function downImg(){
 	console.log('downImg');
 	connection.query(
-		'SELECT id, title FROM amazon.xunleitai  where clazz<>"cl"; ', function(error, results, fields) {
+		'SELECT id, title FROM '+db_table+'  where clazz is  null or clazz<>"ka"; ', function(error, results, fields) {
 			if (error) {
 				console.log("select Error: " + error.message);
 				connection.end();
@@ -186,7 +206,7 @@ function downImg(){
 			for(var a in results) {
 				var row = results[a];
 				if(row.title) {
-					rows.push({id:row.id, uri:'/j/subject_suggest?q='+encodeURI(row.title)});
+					rows.push({id:row.id, uri:/*'https://movie.douban.com*/'/j/subject_suggest?q='+encodeURI(row.title)});
 				}
 			}
 			
