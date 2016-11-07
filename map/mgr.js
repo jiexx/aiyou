785846b3b42132aab235a64c35772d4caf5f774a
unknown <jluo.Roger@jluo-new1.qinmaohao.com>
2016-11-07 20:33:41 +0800

    bing map

--- map/mgr.js
+++ map/mgr.js
@@ -16,63 +16,102 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
-
+var Timeouter =  {
+	start: function () {
+		console.log(""+new Date().getTime()+"    start-----------> "+(new Date().getTime() - this.begin));
+		var _this = this;
+		this.interval = setInterval(function() {
+			if ( (new Date().getTime() - _this.begin < _this.maxtimeOutMillis)) {
+				_this.testFx();
+			} else {
+				console.log(""+new Date().getTime()+"    onTimeout "+(new Date().getTime() - _this.begin)+"  "+_this.begin);
+				_this.onTimeout();
+				clearInterval(_this.interval);
+			}
+		}, 1000); //< repeat check every 250ms
+	},
+	
+	stop: function() {
+		console.log(""+new Date().getTime()+"    clearInterval-----------> "+(new Date().getTime() - this.begin));
+		clearInterval(this.interval);
+	},
+	create: function(testFx, onTimeout) {
+		function F() {};
+		F.prototype = Timeouter;
+		var f = new F();
+		f.interval = null;
+		f.begin = new Date().getTime();
+		f.maxtimeOutMillis = 10000;
+		f.testFx = testFx;
+		f.onTimeout = onTimeout;
+		return f;
+	}
+};
+function render(clipRect, type, ph, page, res) {
+	console.log("render "+clipRect.height+' '+clipRect.top + ' ' + clipRect.width + ' ' + clipRect.left);
+	page.property('clipRect', {
+		top:    clipRect.top,
+		left:   clipRect.left,
+		width:  clipRect.width,
+		height: clipRect.height
+	});
+	page.renderBase64('JPG').then(function(base64){ 
+		console.log("JPG."+base64.constructor.name);
+		if(type=='64') {
+			console.log("64."+base64.constructor.name);
+			
+			res.send(base64);
+		}else {
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write('<html><body><img src="data:image/jpeg;base64,')
+			res.write(base64);
+			res.end('"/></body></html>');
+			console.log("other."+base64.constructor.name);
+		}
+		page.close();
+		//console.log("exit."+base64.constructor.name);
+		ph.exit();
+	});
+};
 function capture(url, res, type) {
 	console.log(url);
 	phantom.create().then(function (ph) {
 		ph.createPage().then(function (page) {
+			console.log(""+new Date().getTime()+"    createPage ");
+			var timeouter = Timeouter.create(function () {
+					console.log(""+new Date().getTime()+"    evaluate ");
+					page.evaluate(function() {
+						console.log(""+new Date().getTime()+"    check1 "+document.querySelector('.MicrosoftMapDrawing'));
+						if(document.querySelector('#zoom') != null) {
+							if (typeof window.callPhantom === 'function') {
+								var clipRect = document.querySelector('.MicrosoftMapDrawing').getBoundingClientRect(); 
+								console.log("callPhantom "+JSON.stringify(clipRect));
+								window.callPhantom(clipRect);
+							}
+						}
+					});
+				},function(){
+					page.close();
+					ph.exit();
+					res.send("");
+				}
+			);
 			page.on('onConsoleMessage', function (msg) {
 				console.log(msg);
 			});
 			page.on('onResourceRequested', function (requestData, networkRequest) {
-				//console.log(requestData.url); // this would push the url into the urls array above
+				//console.log(""+new Date().getTime()+"    "+requestData.url); // this would push the url into the urls array above
 				//networkRequest.abort(); // This will fail, because the params are a serialized version of what was provided
 			});
-			
-			page.open(url).then(function (status) {
-				var start = new Date().getTime();
-				page.evaluate(function() {
-					//console.log(document.querySelector('.MicrosoftMapDrawing'));
-					if ( document.querySelector('svg') ) {
-						//console.log(document.querySelector('.MicrosoftMapDrawing').getBoundingClientRect());
-						return document.querySelector('svg').getBoundingClientRect();
-					}
-					else {
-						//console.log("null...");
-						return null;
-					}
-				}).then(function(clipRect){
-					if(!clipRect) {
-						evaluate();
-					}
-					console.log(clipRect.height+' '+clipRect.top + ' ' + clipRect.width + ' ' + clipRect.left);
-					page.property('clipRect', {
-						top:    clipRect.top,
-						left:   clipRect.left,
-						width:  clipRect.width,
-						height: clipRect.height
-					});
-					page.renderBase64('JPG').then(function(base64){ 
-						setTimeout(function(){
-							console.log("JPG."+base64.constructor.name);
-							if(type=='64') {
-								console.log("64."+base64.constructor.name);
-								
-								res.send(base64);
-							}else {
-								res.writeHead(200, {'Content-Type': 'text/html'});
-								res.write('<html><body><img src="data:image/jpeg;base64,')
-								res.write(base64);
-								res.end('"/></body></html>');
-								console.log("other."+base64.constructor.name);
-							}
-							
-							page.close();
-							//console.log("exit."+base64.constructor.name);
-							ph.exit();
-						}, 8000)
-					});
-				});
+			page.on('onCallback', function (data) {
+				console.log(""+new Date().getTime()+"    onCallback "+data.constructor.name);
+				timeouter.stop();
+				setTimeout(function(){
+					render(data, type, ph, page, res);
+				},1000);
+			});
+			page.open(url).then(function(){
+				timeouter.start();
 			});
 		});
 	});
